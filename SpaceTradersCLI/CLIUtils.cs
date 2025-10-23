ae31ac31fafa88c8837e1e64fe3754d019933fd8
Chase Pierce <capierc3@asu.edu>
2025-10-22 23:51:00 -0400

    CLI animation

---
+++ SpaceTradersCLI/CLIUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTradersCLI
+{
+    internal class CLIUtils
+    {
+        public static async Task TypeLineAsync(string text, int delay = 30)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                await Task.Delay(delay); // milliseconds per character
+            }
+            Console.WriteLine();
+        }
+        public static void ClearScreenLineByLine()
+        {
+            int lines = Console.WindowHeight;
+            for (int i = 0; i < lines; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Thread.Sleep(20); // delay for fade effect
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+        public static void SlideUpClear()
+        {
+            int height = Console.WindowHeight;
+            for (int i = 0; i < height; i++)
+            {
+                Console.MoveBufferArea(0, 1, Console.WindowWidth, height - 1, 0, 0);
+                Thread.Sleep(30);
+            }
+        }
+
+        public static void DissolveClear()
+        {
+            Random rand = new Random();
+            string[] buffer = new string[Console.WindowHeight];
+
+            for (int i = 0; i < Console.WindowHeight; i++)
+            {
+                buffer[i] = new string(' ', Console.WindowWidth);
+            }
+
+            int remaining = Console.WindowHeight;
+            while (remaining > 0)
+            {
+                int line = rand.Next(Console.WindowHeight);
+                if (buffer[line] != null)
+                {
+                    Console.SetCursorPosition(0, line);
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    buffer[line] = null;
+                    remaining--;
+                    Thread.Sleep(10);
+                }
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
--- SpaceTradersCLI/Program.cs
+++ SpaceTradersCLI/Program.cs
@@ -1,14 +1,26 @@
 ﻿using IO.Swagger.Api;
 using IO.Swagger.Model;
+using SpaceTradersCLI;
 
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await CLIUtils.TypeLineAsync("Initializing SpaceTraders CLI...");
+        Agent agent = LoginAndFetchAgentData();
+        CLIUtils.SlideUpClear();
+        await CLIUtils.TypeLineAsync($"Welcome back, {agent.Symbol}!");
+        await CLIUtils.TypeLineAsync($"Your current credits: {agent.Credits}");
+        await CLIUtils.TypeLineAsync("Fetching your fleet data...");
+        String input = Console.ReadLine();
 
-Console.WriteLine("Logging in... Please Wait");
-AgentsApi agentsApi = new AgentsApi();
-agentsApi.Configuration.AccessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiV0lMTF9OT1JUT04iLCJ2ZXJzaW9uIjoidjIuMy4wIiwicmVzZXRfZGF0ZSI6IjIwMjUtMTAtMTkiLCJpYXQiOjE3NjExNzYwNTgsInN1YiI6ImFnZW50LXRva2VuIn0.D_11yzfvv3ciDMfirW1cJwo--gZsYLPpDKmyOTiplNoRo3Xxt3mXW-cdG1qbxliuh1Fqxlo6cy50LwpTYPI9Gt29SYZfSaGhKPoJOP0zuA4H9lrmtj8jkibDZZSQ5dNNXGGN81g8asr_e2s38QaQ72G-bJx3eZYHvh1Lcka53uedBjLM942qGqs-0kQMI_hYMR2NMN6GpPXYTDAE-jeQ0pfzf3EAQ1Dcm25JoPnNQZ334MtEcflPNMJ8ZZf8FolLE16Ev5krjVGPu45F0gdfHfIlV83zxHrS0uwsDvkPfNVH1bfzdBRqQ-SAT1CtxyAxr-H1PfMwAyX2vAgaVT5u8U7y4PzzZgfC5a8DJMAub9_P2A7MHB7SBEeBQgdcbFRk5Z5aIrtehKBgOaQQsj9EFS5G8FNZVLqk4Fkrr_CGTTQ3YRser9HvV3Ymuon2EsWqqXNP9qxWPtAITWWjbORqoI1YHDsusHLfHX6_1JiSBbUC0Ous2GuOarnc6aXhgzxNNEU9EFBzqLkKoMTvjlMxhGPjno7nVSwNjzSc7L5WyzemDjRyNyxEcR5RN15ffLGMhJOgF1QCAeab7tphCzVWEbr8TjfPZT4KlffvObW-QgfUAmUXEJQkf4O_jDFwyFe3IR9H9Li7eFNtUGDJ1kY8UFS5KO4D-ngtCEdG1dEWlgg";
-InlineResponse20011 response = agentsApi.GetMyAgent();
-Agent agent = response.Data;
-Console.Clear();
-Console.WriteLine("Hello, " + agent.Symbol);
-Console.WriteLine("--------------------------");
-Console.WriteLine("What would you like to do?");
-string input = Console.ReadLine();
+    }
+    static Agent LoginAndFetchAgentData()
+    {
+        AgentsApi agentsApi = new AgentsApi();
+        agentsApi.Configuration.AccessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiV0lMTF9OT1JUT04iLCJ2ZXJzaW9uIjoidjIuMy4wIiwicmVzZXRfZGF0ZSI6IjIwMjUtMTAtMTkiLCJpYXQiOjE3NjExNzYwNTgsInN1YiI6ImFnZW50LXRva2VuIn0.D_11yzfvv3ciDMfirW1cJwo--gZsYLPpDKmyOTiplNoRo3Xxt3mXW-cdG1qbxliuh1Fqxlo6cy50LwpTYPI9Gt29SYZfSaGhKPoJOP0zuA4H9lrmtj8jkibDZZSQ5dNNXGGN81g8asr_e2s38QaQ72G-bJx3eZYHvh1Lcka53uedBjLM942qGqs-0kQMI_hYMR2NMN6GpPXYTDAE-jeQ0pfzf3EAQ1Dcm25JoPnNQZ334MtEcflPNMJ8ZZf8FolLE16Ev5krjVGPu45F0gdfHfIlV83zxHrS0uwsDvkPfNVH1bfzdBRqQ-SAT1CtxyAxr-H1PfMwAyX2vAgaVT5u8U7y4PzzZgfC5a8DJMAub9_P2A7MHB7SBEeBQgdcbFRk5Z5aIrtehKBgOaQQsj9EFS5G8FNZVLqk4Fkrr_CGTTQ3YRser9HvV3Ymuon2EsWqqXNP9qxWPtAITWWjbORqoI1YHDsusHLfHX6_1JiSBbUC0Ous2GuOarnc6aXhgzxNNEU9EFBzqLkKoMTvjlMxhGPjno7nVSwNjzSc7L5WyzemDjRyNyxEcR5RN15ffLGMhJOgF1QCAeab7tphCzVWEbr8TjfPZT4KlffvObW-QgfUAmUXEJQkf4O_jDFwyFe3IR9H9Li7eFNtUGDJ1kY8UFS5KO4D-ngtCEdG1dEWlgg";
+        InlineResponse20011 response = agentsApi.GetMyAgent();
+        return response.Data;
+    }
+
+}
