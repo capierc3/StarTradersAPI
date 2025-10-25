e0b0481311d2316be56c33eebadfcc16eff60c27
Chase Pierce <capierc3@asu.edu>
2025-10-25 19:12:11 -0400

    Menu screen with key control

--- SpaceTradersCLI/CLIUtils.cs
+++ SpaceTradersCLI/CLIUtils.cs
@@ -11,6 +11,7 @@ namespace SpaceTradersCLI
     {
         private const int Delay = 25;
         private const string FontColor = "bold green";
+        private const int TopLine = 14;
 
         public static async Task Type(string text)
         {
@@ -20,23 +21,6 @@ namespace SpaceTradersCLI
                 await Task.Delay(Delay);
             }
             Console.WriteLine();
-        }
-
-        public static async Task DisplayLoadingAsync(CancellationToken token)
-        {
-            var sequence = new[] { ".", "..", "...", "..", "." };
-            int counter = 0;
-
-            while (!token.IsCancellationRequested)
-            {
-                Console.SetCursorPosition(0, Console.CursorTop);
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.SetCursorPosition(0, Console.CursorTop);
-                Console.Write($"\r{sequence[counter++ % sequence.Length]}");
-                await Task.Delay(300);
-            }
-
-            Console.Write("\r "); // clear spinner
         }
 
         public static async Task LoadingPrint(string text, CancellationToken token)
@@ -53,33 +37,86 @@ namespace SpaceTradersCLI
                 AnsiConsole.Markup($"\r[{FontColor}]{text}{dots}[/]");
                 await Task.Delay(delay);
             }
+            Console.SetCursorPosition(0, Console.CursorTop);
+            AnsiConsole.Markup(new string(' ', 20));
             Console.WriteLine();
         }
 
-        public static async Task printBanner()
+        public static async Task PrintBanner()
         {
             string[] bannerLines = new string[]
             {
-                "+=============================================+",
-                "|   _____                                     |",
-                "|  / ___/ ____   ____ _ _____ ___             |",
-                "|  \\__ \\ / __ \\ / __ `// ___// _ \\            |",
-                "| ___/ // /_/ // /_/ // /__ /  __/            |",
-                "|/____// .___/ \\__,_/ \\___/ \\___/             |",
-                "|     /_/                                     |",
-                "|  ______                  __                 |",
-                "| /_  __/_____ ____ _ ____/ /___   _____ _____|",
-                "|  / /  / ___// __ `// __  // _ \\ / ___// ___/|",
-                "| / /  / /   / /_/ // /_/ //  __// /   (__  ) |",
-                "|/_/  /_/    \\__,_/ \\__,_/ \\___//_/   /____/  |",
-                "+=============================================+",
-
-
+                "+==========================================================+",
+                "|   _____                                                  |",
+                "|  / ___/ ____   ____ _ _____ ___                          |",
+                "|  \\__ \\ / __ \\ / __ `// ___// _ \\                         |",
+                "| ___/ // /_/ // /_/ // /__ /  __/                         |",
+                "|/____// .___/ \\__,_/ \\___/ \\___/                          |",
+                "|     /_/                                                  |",
+                "|               ______                  __                 |",
+                "|              /_  __/_____ ____ _ ____/ /___   _____ _____|",
+                "|               / /  / ___// __ `// __  // _ \\ / ___// ___/|",
+                "|              / /  / /   / /_/ // /_/ //  __// /   (__  ) |",
+                "|             /_/  /_/    \\__,_/ \\__,_/ \\___//_/   /____/  |",
+                "+==========================================================+",
             };
             foreach (var line in bannerLines)
             {
                 AnsiConsole.MarkupLine($"[{FontColor}]{line}[/]");
                 await Task.Delay(100);
+            }
+        }
+
+        public static async Task PrintMenu(String[] menuItems)
+        {
+            AnsiConsole.Cursor.Hide();
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                if (i == 0)
+                {
+                    await Type($"(*) {menuItems[i]}");
+                }
+                else
+                    await Type($"( ) {menuItems[i]}");
+            }
+        }
+        
+        public static void RefreshMenu(String[] menuItems, int selectedIndex)
+        {
+            AnsiConsole.Cursor.SetPosition(0, Console.CursorTop - (menuItems.Length - 1));
+            AnsiConsole.Cursor.Hide();
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                if (i == selectedIndex)
+                {
+                    AnsiConsole.Markup($"[{FontColor}](*) {menuItems[i]}[/]");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    AnsiConsole.Markup($"[{FontColor}]( ) {menuItems[i]}[/]");
+                    Console.WriteLine();
+                }
+                    
+            }
+        }
+
+        public static void ClearScreen()
+        {
+            while (Console.CursorTop > TopLine)
+            {
+                AnsiConsole.Cursor.MoveUp(1);
+                for (int i = 50; i >= 0; i--)
+                {
+                    Console.SetCursorPosition(i, Console.CursorTop);
+                    Console.Write(" ");
+                    if (i > 0)
+                    {
+                        Console.SetCursorPosition(i - 1, Console.CursorTop);
+                    }
+                    Thread.Sleep(5);
+                }
+                Thread.Sleep(25);
             }
         }
     }
--- SpaceTradersCLI/Program.cs
+++ SpaceTradersCLI/Program.cs
@@ -6,22 +6,81 @@ using System.Threading.Tasks;
 
 class Program
 {
+    static SpaceTradersAPIHelper apiHelper = new SpaceTradersAPIHelper();
+    static ConsoleKey key;
     static async Task Main(string[] args)
     {
-        SpaceTradersAPIHelper apiHelper = new SpaceTradersAPIHelper();
-
         AnsiConsole.Cursor.Hide();
+        Console.CursorVisible = false;
         Console.Title = "SpaceTraders Hub";
-        await CLIUtils.printBanner();
+        await CLIUtils.PrintBanner();
+        await StartUp();
+    }
+
+    static async Task StartUp()
+    {
         var cts = new CancellationTokenSource();
         var spinnerTask = CLIUtils.LoadingPrint("INITIALIZING!", cts.Token);
         Agent agent = apiHelper.GetAgentData();
+        cts.Cancel();
+        await spinnerTask;
+        await CLIUtils.Type($"WELCOME BACK AGENT {agent.Symbol}!");
+        await CLIUtils.Type($"CURRENT CREDITS: {agent.Credits}!");
+        string[] menuOptions = new string[]
+        {
+            "View Fleet",
+            "View Contracts",
+            "Exit"
+        };
+        await CLIUtils.PrintMenu(menuOptions);
+        int selectedIndex = 0;
+        while (true)
+        {
+            key = Console.ReadKey(true).Key;
+            switch(key)
+            {
+                case ConsoleKey.Enter:
+                    if (selectedIndex == 0)
+                    {
+                        CLIUtils.ClearScreen();
+                        ViewFleet();
+                    }
+                    else if (selectedIndex == 1)
+                    {
+                        //View Contracts
+                    }
+                    else if (selectedIndex == 2)
+                    {
+                        CLIUtils.ClearScreen();
+                        await Exit();
+                        return;
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    selectedIndex--;
+                    if (selectedIndex < 0)
+                    {
+                        selectedIndex = menuOptions.Length - 1;
+                    }
+                    CLIUtils.RefreshMenu(menuOptions, selectedIndex);
+                    break;
+                case ConsoleKey.DownArrow:
+                    selectedIndex = (selectedIndex + 1) % menuOptions.Length;
+                    CLIUtils.RefreshMenu(menuOptions, selectedIndex);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    static async void ViewFleet()
+    {
+        var cts = new CancellationTokenSource();
+        var spinnerTask = CLIUtils.LoadingPrint("FETCHING FLEET DATA...!", cts.Token);
         List<Ship> fleet = apiHelper.getFleetData();
         cts.Cancel();
         await spinnerTask;
-        AnsiConsole.Clear();
-        await CLIUtils.Type($"WELCOME BACK AGENT {agent.Symbol}!");
-        await CLIUtils.Type($"CURRENT CREDITS: {agent.Credits}!");
         var table = new Table();
         table.ShowRowSeparators();
         table.Border(TableBorder.Ascii2);
@@ -49,10 +108,60 @@ class Program
                 }
             });
 
-
+        string[] menuOptions =
+        [
+            "Main Menu",
+            "View Contracts",
+            "Exit"
+        ];
+        await CLIUtils.PrintMenu(menuOptions);
+        int selectedIndex = 0;
         while (true)
         {
-
+            key = Console.ReadKey(true).Key;
+            switch (key)
+            {
+                case ConsoleKey.Enter:
+                    if (selectedIndex == 0)
+                    {
+                        CLIUtils.ClearScreen();
+                        await StartUp();
+                    }
+                    else if (selectedIndex == 1)
+                    {
+                        //View Contracts
+                    }
+                    else if (selectedIndex == 2)
+                    {
+                        CLIUtils.ClearScreen();
+                        await Exit();
+                        return;
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    selectedIndex--;
+                    if (selectedIndex < 0)
+                    {
+                        selectedIndex = menuOptions.Length - 1;
+                    }
+                    CLIUtils.RefreshMenu(menuOptions, selectedIndex);
+                    break;
+                case ConsoleKey.DownArrow:
+                    selectedIndex = (selectedIndex + 1) % menuOptions.Length;
+                    CLIUtils.RefreshMenu(menuOptions, selectedIndex);
+                    break;
+                default:
+                    break;
+            }
         }
+    }
+
+
+
+    static async Task Exit()
+    {
+        await CLIUtils.Type("GOOD BYE AGENT...");
+        await Task.Delay(1500);
+        Environment.Exit(0);
     }
 }
